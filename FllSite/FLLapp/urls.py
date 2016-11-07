b059d4f70372a8f42d6ad4c277878b41b1b84ce1
ebomberger23 <ebomberger@gmail.com>
2016-11-07 15:34:19 -0500

    Update urls.py

--- FllSite/FLLapp/urls.py
+++ FllSite/FLLapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
         url(r'^aboutcenter(?P<centerid>[0-9]+)/',views.aboutcenter,name="aboutcenter"),
         url(r'^searchresults',views.searchresults,name="searchresults"),
         url(r'^search$',views.search,name='search'),
-        url(r'^aboutlab(?P<labid>[0-9]+)/',views.aboutlab,name="aboutcenter")
+        url(r'^aboutlab(?P<labid>[0-9]+)/',views.aboutlab,name="aboutlab"),
+        url(r'^animal(?P<animalid>[0-9]+)/',views.animal,name="animal")
 ]
 
