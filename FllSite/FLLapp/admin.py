857d891844b882c4185ed455d029c447ef31b4fd
ebomberger23 <ebomberger@gmail.com>
2016-11-11 16:05:41 -0500

    Update admin.py

--- FllSite/FLLapp/admin.py
+++ FllSite/FLLapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Center,Animal
+from .models import Lab,Center,Animal
 # Register your models here.
 admin.site.register(Center)
+admin.site.register(Lab)
 admin.site.register(Animal)
